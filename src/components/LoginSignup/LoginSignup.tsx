326b41f95f575906ceb05931c5d12475adc2f55f
Cole-Wright4195 <132401722+Cole-Wright4195@users.noreply.github.com>
2024-11-09 21:34:54 -0500

    fixed evironment css

---
+++ src/components/LoginSignup/LoginSignup.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from './LoginSignup.module.css'
+import './LoginSignup.module.css'
+import person from '../Assets/person.png'
+import email from '../Assets/email.png'
+import password from '../Assets/password.png'
+import bulldog from '../Assets/bulldog.png'
+
+
+const LoginSignup = () =>{
+    return (
+        <div className={styles.bulldog}>
+            <img src={bulldog.src} width={200} alt=""/>
+            
+        <div className= {styles.container}>
+            <div className= {styles.header}>
+                <div className={styles.text}>askUGA</div>
+                <div className={styles.underline}></div>
+
+            </div>
+            <div className= {styles.inputs}>
+
+                <div className={styles.input}>
+                    <img src= {email.src} width={20} alt=""/>
+                    <input type ="email" placeholder='email'/>
+                </div>
+
+                <div className={styles.input}>
+                    <img src= {password.src} width={20} alt=""/>
+                    <input type ="password" placeholder='password'/>
+                </div>
+            </div>
+            <div className={styles.loginsubmit}>
+            <div className={styles.submit}>Login</div>
+            </div>
+        </div>
+        <div className={styles.submitcontainer}>
+            <div className={styles.signup}><span>Sign-Up</span></div>
+            <div className={styles.forgotpassword}><span>Forgot-Password?</span></div>
+          
+            </div>
+       </div>
+    )
+}
+
+export default LoginSignup
