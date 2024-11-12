b4de8a6b2f4e0fc71b680ed0604ae5c8476e0e10
khand25 <drk46099@uga.edu>
2024-11-12 12:07:43 -0500

    Update CreatePost.tsx

--- src/app/components/CreatePost/CreatePost.tsx
+++ src/app/components/CreatePost/CreatePost.tsx
@@ -1,17 +1,59 @@
+"use client"
 import React from "react";
 import styles from "./CreatePost.module.css";
+import {useState} from 'react';
+import { useRouter } from "next/navigation";
+import { document } from "postcss";
 
 
 function CreateNewPost() {
+
+type postProp  = {
+    username: string;
+    title: string;
+    description: string;
+};
+
+const handleClearInput = () => {
+    setDescription("");
+}
+
+
+const router = useRouter();
+
+//State varaibles for form inputs
+const [title, setTitle] = useState<string>("");
+const [description, setDescription] = useState<string>("");
+
+//Handle change for title input
+const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+};
+
+//Handle change ofr description input
+const handleDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDescription(event.target.value);
+};
+
+// Handle form submission
+const handleSubmit = () => {
+    //event.preventDefault(); // Prevent the default form submission behavior
+    console.log("Title:", title);
+    console.log("Description:", description);
+};
+
+
+
+
     return (
         <div>
             <h1 className={styles.startingText}> Create a new post</h1>
             <div className={styles.generalInputBox}>
                 <form id={styles.outerform}>
                     
-                    <input type="text" id={styles.firstTextBox}name="firstbox" placeholder=" Title here..."/>
+                    <input type="text" id={styles.firstTextBox}name="firstbox" placeholder=" Title here..." value={title} onChange={handleTitleChange}/>
                     <div className={styles.text2}>
-                    <input type="text" id={styles.secondTextBox} name="secondbox" placeholder="Description here..."/> 
+                    <input type="text" id={styles.secondTextBox} name="secondbox" placeholder="Description here..." value={description} onChange={handleDescriptionChange}/> 
                     </div> 
                    
                 </form>
@@ -20,7 +62,7 @@ function CreateNewPost() {
             <button id={styles.userButton}>
                 DK
             </button>
-            <button id={styles.postButton}> 
+            <button  id={styles.postButton} onClick={handleSubmit}> 
                 Post
             </button>
         </div>
